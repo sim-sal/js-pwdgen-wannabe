5a7f0464d22bbde11ee6a0d0f8c2845fe2969d99
sim-sal <simone.saladino93@gmail.com>
2023-04-27 01:15:04 +0200

    added html, css properties and js variables

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// dichiaro le variabili
+let nomeUtente, cognomeUtente, coloreUtente, password, messaggio;
+
+// Chiedere all’utente il suo nome 
+nomeUtente = prompt("inserisci il tuo nome");
+console.log(nomeUtente);
+
+// chiedere il suo cognome
+cognomeUtente = prompt("Inserisci il tuo cognome");
+console.log(cognomeUtente);
+
+// chiedere il suo colore preferito
+coloreUtente = prompt("Inserisci il tuo colore preferito");
+console.log(coloreUtente);
+
+// creo password
+password = nomeUtente + cognomeUtente + coloreUtente + 21;
+console.log(password);
+
+// creo messaggio per utente
+messaggio = `La tua password generata è ${password}`;
+console.log(messaggio);
+
+// infine, riportare il tutto sulla pagina
+document.getElementById("mio_id").innerHTML = messaggio;
+
+
+
+
+
+
